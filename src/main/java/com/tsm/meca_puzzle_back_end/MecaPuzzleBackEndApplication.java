2954c4f70cedba47b5f5c61279ee32f9ad909445
THIAGO MOYSES <thiagodasilvamoysesfr@gmail.com>
2025-12-30 14:37:06 +0100

    Initial Spring Boot project setup
    Set up a new Spring Boot project for the meca-puzzle-back-end application. Added Maven wrapper, project structure, basic dependencies for Spring Boot, JPA, Security, Validation, Web, and PostgreSQL. Included initial application properties for database configuration and a basic test class.

---
+++ src/main/java/com/tsm/meca_puzzle_back_end/MecaPuzzleBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.tsm.meca_puzzle_back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MecaPuzzleBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MecaPuzzleBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tsm/meca_puzzle_back_end/MecaPuzzleBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsm.meca_puzzle_back_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MecaPuzzleBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
